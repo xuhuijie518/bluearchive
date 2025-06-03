c46da1140ce346ed80ec03d02c1d73039b11fed4
xuhuijie <sheepwind0518@163.com>
2025-06-03 14:22:36 +0800

    update

--- vite.config.js
+++ vite.config.js
@@ -15,7 +15,8 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
-  base: process.env.NODE_ENV === 'production' 
-    ? '/bluearchive/' 
-    : '/',
+  base: '/bluearchive/' ,
+  // build: {
+  //   outDir: "docs"
+  // }
 })
